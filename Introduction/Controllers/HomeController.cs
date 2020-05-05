6bc36f8135cdb15d7eb81ed3348dc10a7cb0d00c
Diego Harrison <diegoharrisonsm@gmail.com>
2020-05-05 00:36:38 -0300

    first commit

---
+++ Introduction/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Introduction.Models;
+using System.Web.Mvc;
+
+namespace Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var person = new Person()
+            {
+                PersonId = 1,
+                Name = "Harrison",
+                Type = "TI"
+            };
+
+            ViewData["PessoaId"] = person.PersonId;
+            ViewData["Name"] = person.Name;
+            ViewData["Type"] = person.Type;
+
+            ViewBag.personId = person.PersonId;
+            ViewBag.name = person.Name;
+            ViewBag.type = person.Type;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Introduction/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Introduction.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
